09613c7003393d4e43f20b2fa91be4fae1d9a3f3
sabexzero <sabexqe@yandex.ru>
2023-08-27 22:32:36 +0300

    Created service for passengers and very very thin controller with logs and mapper

---
+++ ShuttleApi.PassengerMicroservice/Common/Utilities/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ShuttleApi.PassengerMicroservice.Data.DTOs;
+using ShuttleApi.PassengerMicroservice.Models;
+
+namespace ShuttleApi.ShuttleMicroservice.Common.Utilities
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<PassengerDTO, Passenger>();
+            CreateMap<Passenger, PassengerDTO>();
+        }
+    }
+}
--- ShuttleApi.PassengerMicroservice/Controllers/PassengerController.cs
+++ ShuttleApi.PassengerMicroservice/Controllers/PassengerController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+using ShuttleApi.PassengerMicroservice.Data.DTOs;
+using ShuttleApi.PassengerMicroservice.Services;
 using ShuttleApi.PassengerMicroservice.Services.Contracts;
 
 namespace ShuttleApi.UserService.Controllers
@@ -8,12 +11,33 @@ namespace ShuttleApi.UserService.Controllers
     public class PassengerController : Controller
     {
         private readonly IPassengerService _passengerService;
-        //логиирование добавить
+        //логгирование будет использоваться для ошибок, так как логи о создании, удалении объекта и тд вшиты в сервис
+        private readonly Logger _logger;
         public PassengerController(IPassengerService passengerService)
         {
             _passengerService = passengerService;
-            //здесь будет логи
+            _logger = LogManager.GetCurrentClassLogger();
         }
-        //дальше методы
+
+        [HttpPost("CreatePassenger")]
+        public async Task<ActionResult> CreatePassenger([FromBody] PassengerDTO passengerDto, [FromBody] string password)
+        {
+            await _passengerService.CreatePassenger(passengerDto, password);
+            return Ok();
+        }
+
+        [HttpGet("GetPassengerById")]
+        public async Task<ActionResult<PassengerDTO>> GetShuttleById([FromBody] string shuttleId)
+        {
+            var passenger = await _passengerService.GetPassengerById(shuttleId);
+            return Ok(passenger);
+        }
+
+        [HttpGet("GetAllPassengers")]
+        public async Task<ActionResult<IEnumerable<PassengerDTO>>> GetAllPassengers()
+        {
+            var listOfPassengers = await _passengerService.GetAllPassengers();
+            return Ok(listOfPassengers);
+        }
     }
 }

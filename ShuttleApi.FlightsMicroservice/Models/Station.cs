18fe85ea6c21ed21a7ef8a29405a4c89f5fe10f4
sabexzero <sabexqe@yandex.ru>
2023-08-28 23:26:42 +0300

    smthg changes in flights service

--- ShuttleApi.FlightsMicroservice/Models/Station.cs
+++ ShuttleApi.FlightsMicroservice/Models/Station.cs
@@ -3,12 +3,12 @@
     public class Station // их должно быть минимум две
                         //чтобы планета была пригодная для полетов, такие правила
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public DateTimeOffset CreatedAt { get; set; }
         public string Title { get; set; } 
         public int RefuelingSpeed { get; set; } // per min
         public int NumberOfShipsNow { get; set; }
         public int MaxNumberOfShips { get; set; }
-        public Guid PlanetId { get; set; }
+        public string PlanetId { get; set; } = string.Empty;
      }
 }
